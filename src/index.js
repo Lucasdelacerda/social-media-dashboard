e96da2695360c7f7e1c9f560e1ee3c6314a24a92
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2024-07-16 17:58:17 -0300

    API include

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const backgroundToggle = document.querySelector('.background-toggle-change');
 let corpo = document.querySelector('body');
 let mode = document.querySelector('.mode-change');
 let cards = document.querySelectorAll('.cards');
+const youtubeSubscribers = document.querySelector('.youtube-subscribers');
 mode.innerText = 'Dark mode';
 
 backgroundToggle.addEventListener('click', () => {
@@ -26,3 +27,29 @@ backgroundToggle.addEventListener('click', () => {
     mode.innerText = 'Dark mode';
   }
 })
+
+const apiKey = 'AIzaSyDBo0A4ecm9u1_KvwsauJI-ZxAZWFQU3zo';
+const channelId = 'UC5PBnSG7C0WXp5gjd4bzKtw';
+const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.items && data.items.length > 0) {
+      const statistics = data.items[0].statistics;
+      console.log(`Inscritos: ${statistics.subscriberCount}`);
+      console.log(`Visualizações: ${statistics.viewCount}`);
+      if (statistics.subscriberCount > 1000000) {
+        const formattedNumber = Math.floor(statistics.subscriberCount / 1000000)
+        youtubeSubscribers.innerText = `${formattedNumber}kk`;
+      } else if (statistics.subscriberCount > 10000) {
+        const formattedNumber = Math.floor(statistics.subscriberCount / 1000)
+        youtubeSubscribers.innerText = `${formattedNumber}k`;
+      } else {
+        youtubeSubscribers.innerText = statistics.subscriberCount;
+      }
+    } else {
+      console.log('Canal não encontrado');
+    }
+  })
+  .catch(error => console.error('Erro ao buscar dados do canal:', error));
